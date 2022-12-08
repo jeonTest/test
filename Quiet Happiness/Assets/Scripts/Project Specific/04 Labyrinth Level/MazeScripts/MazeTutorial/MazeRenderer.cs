7d3c731e47c2d9658ac336266bd28ff810b0c818
jeonmaxx <jazuendorf@stud.mediadesign.de>
2022-12-08 01:09:10 +0100

    Labyrinth walls updated

--- Quiet Happiness/Assets/Scripts/Project Specific/04 Labyrinth Level/MazeScripts/MazeTutorial/MazeRenderer.cs
+++ Quiet Happiness/Assets/Scripts/Project Specific/04 Labyrinth Level/MazeScripts/MazeTutorial/MazeRenderer.cs
@@ -45,15 +45,15 @@ public class MazeRenderer : MonoBehaviour
                 if (cell.HasFlag(WallState.UP))
                 {
                     var topWall = Instantiate(wallPrefab, transform) as Transform;
-                    topWall.position = position + new Vector3(0, 0, size / 2);
-                    topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
+                    topWall.position = position + new Vector3(0, (topWall.localScale.y/100), size / 2);
+                    topWall.localScale = new Vector3(topWall.localScale.x, topWall.localScale.y, topWall.localScale.z);
                 }
 
                 if (cell.HasFlag(WallState.LEFT))
                 {
                     var leftWall = Instantiate(wallPrefab, transform) as Transform;
-                    leftWall.position = position + new Vector3(-size / 2, 0, 0);
-                    leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
+                    leftWall.position = position + new Vector3(-size / 2, (leftWall.localScale.y / 100), 0);
+                    leftWall.localScale = new Vector3(leftWall.localScale.x, leftWall.localScale.y, leftWall.localScale.z);
                     leftWall.eulerAngles = new Vector3(0, 90, 0);
                 }
 
@@ -62,8 +62,8 @@ public class MazeRenderer : MonoBehaviour
                     if (cell.HasFlag(WallState.RIGHT))
                     {
                         var rightWall = Instantiate(wallPrefab, transform) as Transform;
-                        rightWall.position = position + new Vector3(+size / 2, 0, 0);
-                        rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
+                        rightWall.position = position + new Vector3(+size / 2, (rightWall.localScale.y / 100), 0);
+                        rightWall.localScale = new Vector3(rightWall.localScale.x, rightWall.localScale.y, rightWall.localScale.z);
                         rightWall.eulerAngles = new Vector3(0, 90, 0);
                     }
                 }
@@ -73,8 +73,8 @@ public class MazeRenderer : MonoBehaviour
                     if (cell.HasFlag(WallState.DOWN))
                     {
                         var bottomWall = Instantiate(wallPrefab, transform) as Transform;
-                        bottomWall.position = position + new Vector3(0, 0, -size / 2);
-                        bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
+                        bottomWall.position = position + new Vector3(0, (bottomWall.localScale.y / 100), -size / 2);
+                        bottomWall.localScale = new Vector3(bottomWall.localScale.x, bottomWall.localScale.y, bottomWall.localScale.z);
                     }
                 }
             }
