78ab0c4e6bc83dd7a590f1d76f660486f994f648
jeonmaxx <jazuendorf@stud.mediadesign.de>
2022-12-15 21:02:25 +0100

    Einfärben updated
    probiere das einfärben ab jetzt in einer anderen Unity Datei aus, um nichts kaputt zu machen

--- Quiet Happiness/Assets/Scripts/Project Specific/03 Pinsel Level/PaintingTest/ComputeShaderStuff.cs
+++ Quiet Happiness/Assets/Scripts/Project Specific/03 Pinsel Level/PaintingTest/ComputeShaderStuff.cs
@@ -9,16 +9,20 @@ public class ComputeShaderStuff : MonoBehaviour
 
     int width = 512;
     int height = 512;
+    bool isReady = false;
 
     private void Awake()
     {
         rtt = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
         rtt.enableRandomWrite = true;
         rtt.Create();
+        isReady = true;
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        if (!isReady) return;
+        Debug.Log("OnRenderImage");
         int kernel = computeShader.FindKernel("CSMain");
 
         int workgroupsX = Mathf.CeilToInt(width/8);
