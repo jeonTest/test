5b6a60bc302aee751878c0910b15150f93cd4aff
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-01-08 20:20:07 +0100

    Player and Fireflies added

--- Quiet Happiness/Assets/Scripts/Project Specific/04 Labyrinth Level/MazeScripts/Laby2Test.cs
+++ Quiet Happiness/Assets/Scripts/Project Specific/04 Labyrinth Level/MazeScripts/Laby2Test.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Laby2Test : MonoBehaviour
 {
     public Vector3 Labyrinth2Position;
+    public Vector3 rotationVector;
+
     void Update()
     {
         transform.position = Labyrinth2Position;
+        transform.rotation = Quaternion.Euler(rotationVector);
     }
 
 }
