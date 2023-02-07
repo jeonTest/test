1f842724b7da473194e64354e36835da0fd0d542
jeonmaxx <103994264+jeonmaxx@users.noreply.github.com>
2023-02-07 11:58:27 +0100

    small changes

--- Quiet Happiness/Assets/Scripts/Project Specific/03 Pinsel Level/ActiveColor.cs
+++ Quiet Happiness/Assets/Scripts/Project Specific/03 Pinsel Level/ActiveColor.cs
@@ -6,45 +6,69 @@ public class ActiveColor : MonoBehaviour
 {
     public string[] Brush = { "Red", "Orange", "Yellow", "Green", "LightBlue", "DarkBlue", "Purple", "Brown" };
     public string colourActive;
-  
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    public bool brushCursor = false;
+
     public void Red()
     {
         colourActive = Brush[0];
+        brushCursor = true;
     }
 
     public void Orange()
     {
         colourActive = Brush[1];
+        brushCursor = true;
     }
 
     public void Yellow()
     {
         colourActive = Brush[2];
+        brushCursor = true;
     }
 
     public void Green()
     {
         colourActive = Brush[3];
+        brushCursor = true;
     }
 
     public void LightBlue()
     {
         colourActive = Brush[4];
+        brushCursor = true;
     }
 
     public void DarkBlue()
     {
         colourActive = Brush[5];
+        brushCursor = true;
     }
 
     public void Purple()
     {
         colourActive = Brush[6];
+        brushCursor = true;
     }
 
     public void Brown()
     {
         colourActive = Brush[7];
+        brushCursor = true;
+    }
+
+    public void Update()
+    {
+        if(brushCursor == true)
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+        else if(brushCursor == false)
+        {
+            Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        }
     }
 
 
