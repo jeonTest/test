e3ae28d9ac2f512072bbb1fdbd89f24306a23668
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-02-17 20:01:59 +0100

    Preparation for funiture

--- Quiet Happiness/Assets/Scripts/Project Specific/Completion/CompletionIDs.cs
+++ Quiet Happiness/Assets/Scripts/Project Specific/Completion/CompletionIDs.cs
@@ -14,4 +14,16 @@ public static class CompletionIDs
     public const string LABYRINTHLEVELGIVEITEM = "LabyrinthLevelGiveItem";
     public const string BRUSHLEVELDONE = "BrushLevelDone";
     public const string BRUSHLEVELGIVEITEM = "BrushLevelGiveItem";
+
+
+    //Zwischenbelohnungen
+    public const string LABYRINTHONEDONE = "LabyrinthOneDone";
+    public const string LABYRINTHONEGIVEITEM = "LabyrinthOneGiveItem";
+    public const string LABYRINTHTWODONE = "LabyrinthTwoDone";
+    public const string LABYRINTHTWOGIVEITEM = "LabyrinthTwoGiveItem";
+
+    public const string BRUSHONEDONE = "BrushOneDone";
+    public const string BRUSHONEGIVEITEM = "BrushOneGiveItem";
+    public const string BRUSHTWODONE = "BrushTwoDone";
+    public const string BRUSHTWOGIVEITEM = "BrushTwoGiveItem";
 }
