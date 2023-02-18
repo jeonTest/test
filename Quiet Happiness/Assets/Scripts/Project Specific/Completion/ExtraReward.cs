6d6e662bd02a60f5168ea73c1100b7572bae9ac7
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-02-18 21:31:36 +0100

    rewards added Pinselspiel

---
+++ Quiet Happiness/Assets/Scripts/Project Specific/Completion/ExtraReward.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Reflection;
+
+public class ExtraReward : MonoBehaviour
+{
+    public bool playerNear = false;
+    public float interactDist = 5;
+    public PlayerInput Input;
+    public ParticleSystem glitter;
+
+    [DropDown(nameof(IDs))] [SerializeField] public int SelectedIDOnCompletion;
+    [HideInInspector] public List<string> IDs;
+
+    public void Update()
+    {
+        distanceToPlayer();        
+    }
+
+    public void distanceToPlayer()
+    {
+        Transform playerObj = GameObject.FindWithTag("Player").transform;
+        float dist = Vector3.Distance(playerObj.position, transform.position);
+
+        if (dist <= interactDist)
+        {
+            playerNear = true;
+        }
+        else
+        {
+            playerNear = false;
+        }
+    }
+
+    private void OnInteract()
+    {
+        if(playerNear == true)
+        {
+            ModuleManager.GetModule<SaveGameManager>().SetCompletionInfo(IDs[SelectedIDOnCompletion], true);
+            Destroy(glitter);
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (IDs == null)
+        {
+            IDs = new List<string>();
+        }
+        else IDs.Clear();
+        foreach (FieldInfo field in typeof(CompletionIDs).GetFields())
+        {
+            string value = (string)field.GetValue(null);
+            IDs.Add(value);
+        }
+    }
+
+
+}
