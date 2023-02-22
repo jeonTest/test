b0c6ec07b27f7b20c14a960a87db09e8c315df42
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-02-22 17:11:00 +0100

    Steuerung changable

---
+++ Quiet Happiness/Assets/Scripts/Project Specific/Completion/ShowControllsInGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ShowControllsInGame : MonoBehaviour
+{
+    [SerializeField] private string _control;
+    [SerializeField] private string _actionName;
+    [SerializeField] private Text _displayText;
+    private SettingsManager _manager;
+
+
+    void Start()
+    {
+        _manager = ModuleManager.GetModule<SettingsManager>();
+    }
+
+    private void Update()
+    {
+        _displayText.text = _manager.CurrentValueOfControl(_control, _actionName).Split("/").Last();
+    }
+}
